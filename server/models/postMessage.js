26bfb9f0eeeb8988c841b9726549a534c1dff721
elijah-lozano <elijahlozano@hotmail.com>
2021-02-13 13:52:15 -0600

    added post message schema and model

---
+++ server/models/postMessage.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+// define a post schema
+const postSchema = mongoose.Schema({
+   title: String,
+   message: String,
+   creator: String,
+   tags: [String],
+    selectedFile: String,
+    likeCount: {
+       type: Number,
+        default: 0
+    },
+    createdAt: {
+       type: Date,
+        default: new Date()
+    }
+});
+
+// create a mongoose model for post messages
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+// export it for use elsewhere
+export default PostMessage;

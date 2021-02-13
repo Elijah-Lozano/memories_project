90d5cfc2de8300b1118dcb5a4585cf841fdb3505
elijah-lozano <elijahlozano@hotmail.com>
2021-02-13 14:15:40 -0600

    added logic for getting post msgs and creating posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 // this is reached with: locahost:5000/posts
 router.get('/', getPosts);
+router.post('/', createPost);
+
 
 export default router;

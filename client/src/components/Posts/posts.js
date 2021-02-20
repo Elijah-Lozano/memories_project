66487a57f3d74b8c1d2c80086d37d80ef1bcf79e
elijah-lozano <elijahlozano@hotmail.com>
2021-02-20 13:57:04 -0600

    removed console logs

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -5,6 +5,7 @@ import { useSelector } from  'react-redux';
 
 import useStyles from './styles.js';
 const Posts = () => {
+
     const posts =  useSelector((state) => state.posts);
     const classes = useStyles();
 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,12 +2,10 @@ import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
     try{
+
         // since finding all the post messages takes time, it is an async action
         // so we need to add await and make the function async
         const postMessages = await PostMessage.find();
-
-        console.log(postMessages);
-
         res.status(200).json(postMessages);
 
     } catch (error) {
@@ -16,17 +14,15 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
+
     const post = req.body;
-    console.log("Hello2" + post)
     const newPost = new PostMessage(post);
-    console.log(newPost)
+
    try{
-        console.log("about to save new Post")
 
-       await newPost.save();
-
-        console.log("new post saved...")
+        await newPost.save();
         res.status(201).json(newPost);
+
    } catch (error) {
         res.status(409).json({ message: error.message });
    }
